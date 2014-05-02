0df1d35b34307112a0a4c6947194375cbf27beb3
Caitlin <caitlin.riley@students.olin.edu>
2014-05-02 00:03:13 -0400

    final

--- Project/README.txt
+++ Project/README.txt
@@ -1,49 +1,95 @@
-Ariana Chae, Jazmin Gonzalez-Rivero, Caitlin Riley
+Project Description:
 
-To compile:
-	1) Start SML/NJ
+Our team created a dynamic syntax checker for a very basic functional programming language written in SML. We built off of a language we worked with in a previous homework (homework 4) combined with code from lecture 10 and found that the error messages we were getting from the language did not provide us with sufficient information to diagnose syntactical errors that we made. In order to implement a smart syntax checker, we had to change the lexer and parser to be able to handle incomplete programs and programs with newlines, and then take the data and present it in a useful way to help the user identify and correct their mistake. We did this by catching when an error occurred and then presenting the tokens parsed thus far and the tokens left over, showing the user where they missed an expected token.
+
+We ran into some issues with nested functions and with cases where a program could expect multiple types of tokens, and we also had to find a way to store and update the information gathered so as not to lose any useful markers as to where the syntax error occurred. As a result, there are times when a program fails and there are many errors shown. This usually occurs when a given expression type (such as “let”) is used more than twice in the same line. For this project we also wanted to focus on there only being one error at a time. We find that usually at the very least one of the error messages displays a comprehensible error message that can help the user deduce their mistake.
+
+The IDE portion of the dynamic syntax checker is a simple Windows Form written in C#. After the user stops typing for a specific length of time, the program saves what the user has written so far in a plain text file and starts an sml process that tokenizes, parses, and evaluates the contents of that file. The standard output of this process is redirected so that the result of parsing and evaluation can be displayed in the IDE. In the event of a parsing or evaluation error, the first line that generated an error is underlined. Currently the file paths used to start sml and save and check the file are relative to one computer, so it cannot actually be run successfully on another computer without changing those file paths. The code for the IDE can be found in the SimpleIDE folder.
+
+To compile and run our language from the command line:
+	1) Start SML/NJ in the folder containing make.cm
 	2) Type "CM.make "make.cm";"
-	3) Type "Shell.run[];"
-
-Sample outputs:
-
-	Example of errors caught in the parser:
-
-		- Shell.run[];
-		Type . by itself to quit
-		Type :parse <expr> to see the parse of expression <expr>
-		Initial environment: add sub interval hd tl cons nil map filter equal less 
-		project> let double x = x + in double 10
-		Parsing error: error in let - expected expr 
-		project> [1,]
-		Parsing error: error in map or filter - expected bar 
-		error in interval - expected ddots
-
-	Example of errors caught in the evaluator:
-
-		- Shell.run[];
-		Type . by itself to quit
-		Type :parse <expr> to see the parse of expression <expr>
-		Initial environment: add sub interval hd tl cons nil map filter equal less 
-		project> cons 21 meow
-		Evaluation error: There is no function called meow, please check your spelling or your inputs
-		project> hd 20
-		Evaluation error: Error: Not a list
-		project> interval 1 hi
-		Evaluation error: There is no function called hi, please check your spelling or your inputs
-		project> #c {a=10, b=20}
-		Evaluation error: There is no function called c, please check your spelling or your inputs
+	3) Type "Shell.run [];"
 
 
-Project organization:
+Sample executions:
 
-	The bulk of the work done thus far for our project has been trial and error to find the best ways to implement different aspects of our project. Although the final method we have decided on appears quite simple, we have determined that it is the best way to accomplish our goals at the moment. Some methods we have explored include creating our own SOME and NONE option datatypes, trying to have a reference pointer to pass our tokens to automate the process of creating error messages (as opposed to our current option), exploring different places to put our external files to read to (i.e. creating a primitive function in the evaluator), and more. We have also figured out how to use sockets to read/write to and from files.
+(These examples were run through the terminal; however, the outputs would be the same using the more dynamic Windows form.)
 
-	In order to allow for the shell to catch for syntax errors, we have changed the errors in the evaluator to be more descriptive, and we are updating and returning a reference string to specify an error every time a program fails to move on to the next step in a function in the parser. We also pulled code from homework 4 and lecture 10 (the lazy evaluator) to implement better function and variable definitions.
+project> let double x = x + in double 10  
+Parsing error: error in let- expected expr
+let double x = <expr> in double 10  
+x + <term> 
+
+project> let max a = if (a<b) then b else a in max 10 20
+Evaluation error: There is no function called b, please check your spelling or your inputs
+
+project> 1::(2::)
+Parsing error: error in Append List- expected C term
+1 :: <term> 
+( <expr> )  
+2 :: <term> 
+
+project> match (1::) with [] -> 1 | x::xs -> 2
+Parsing error: error in match- expected expr
+match <expr> with [ ] -> 1 | x :: xs -> 2  
+( <expr> ) 
+1 :: <term>
+
+project> match (1::nil) with [] -> 1 x::xs -> 2
+Parsing error: error in match- expected bar
+match ( 1 :: nil ) with [ ] -> 1 x :: xs '|'-> 2 
+
+project> [3,2,4,]
+Parsing error: error in list- expected expr
+[ 3 , 2 , 4 , 'expr'] 
+
+project> interval (1+2) (3+4 
+Parsing error: error in parentheses- expected right parentheses
+( 3 + 4 ')'Function Call Failed 
+
+project> [1..]
+Parsing error: error in interval- expected expr
+[ 1 .. <expr> ] 
+
+project> [(1+2)..(4+)]
+Parsing error: error in interval- expected expr
+[ ( <expr> ) .. ( 4 + ) ]  
+1 + <term> 
+2 ) .. <expr> ]... ) 
+( <expr> ) 
+4 + <term> 
+
+project> [x|x <- [1,2,,4]]
+Parsing error: error in list- expected expr
+[ x | x <- [ 1 , 2 , 'expr', 4 ] ] 
+
+project> [add 10 x | x [1,2,3,4]]
+Parsing error: error in map or filter- expected left arrow
+[ add 10 x | x '<-'[ 1 , 2 , 3 , 4 ] ] 
+
+project> filter (\x true) [1,2,3,4]
+Parsing error: error in function- expected right arrow
+( / x '->'true ) [ 1 , 2 , 3 , 4 ] Function Call Failed
+
+project> [x|x <- [1,2,3,4], tru]
+Evaluation error: There is no function called tru, please check your spelling or your inputs
 
 
-Next steps:
+project> [x + x | x <- [1,2,3,4,5,6,7], x < 3
+Parsing error: error in filter- expected right bracket
+[ x + x | x <- [ 1 , 2 , 3 , 4 , 5 , 6 , 7 ] , x > 3 ']'
 
-	The next step is to find a way to create an environment from within an environment (or something similar) to allow us to dynamically pull someone's file and check it as they type. This means we are moving the user input away from the shell, although it will be still be running in parallel, and moving towards reading programs from an external file which will then be parsed. This will take us one step closer to our goal of creating a dynamic syntax checker for our language's surface syntax.
+project> #a {a = 10, b = }
+Parsing error: error in field- expected expr
+# a <expr> 
+{ <field> } 
 
-	From here, if we have time, we will make more intelligent error messages to help the users more accurately diagnose their problems.
+project> {a=20; b=20}
+Parsing error: cannot tokenize ; b=20}
+
+Things to note:
+
+- We implemented a type checking function (evaluator.sml, line 16) for our primitive types to use for debugging, because there is no easy way in SML to check the type of an object.
+- Throughout evaluator.sml, we made several smaller changes to accommodate for changes we made in how lists are created and changed and how functions are created.
+- Some of our functions were syntactically similar, making it hard to tell which function the user was trying to call when they made an error. (For example, the syntax for map, filter, interval and list all encompass brackets and expressions and are hard to differentiate.) We combined the functions into one giant function (parser.sml, line 697) to accommodate this issue and improve our error messages.
---
+++ Project/SimpleIDE/SimpleIDE/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(592, 508);
+            this.splitContainer1.SplitterDistance = 423;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(568, 407);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.Location = new System.Drawing.Point(10, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(569, 75);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 523);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(593, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(593, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewHelpToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "RTF Files (*.rtf)|*.rtf|Plain Text Files (*.txt)|*.txt";
+            this.openFileDialog1.FilterIndex = 2;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "RTF Files (*.rtf)|*.rtf|Plain Text Files (*.txt)|*.txt";
+            this.saveFileDialog1.FilterIndex = 2;
+            // 
+            // viewHelpToolStripMenuItem
+            // 
+            this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.viewHelpToolStripMenuItem.Text = "View Help";
+            this.viewHelpToolStripMenuItem.Click += new System.EventHandler(this.viewHelpToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 545);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SimpleIDE";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
+
+    }
+}
+
